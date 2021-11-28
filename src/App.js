5c994819409b42fe118814454c9f20ff6225a9cb
mitchelst <mitchel.stroosnijder@gmail.com>
2021-11-28 19:49:47 +0100

    add: bitcoin tracker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [currency, setCurrency] = useState("EUR");
+  const [price, setPrice] = useState(null);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const getBitcoinValue = async () => {
+      try {
+        setLoading(true);
+        setError(false);
+        const response = await fetch(`https://api.coindesk.com/v1/bpi/currentprice/${currency}.json`);
+
+        const data = await response.json();
+        setPrice(data.bpi[currency].rate_float);
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+        setError(true);
+      }
+    };
+
+    getBitcoinValue();
+  }, [currency]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <p className="title">Bitcoin Prijs Tracker</p>
+
+      <p>1 Bitcoin</p>
+      <p>=</p>
+
+      <form>
+        <div className="priceData">
+          <select className="currency" onChange={e => setCurrency(e.target.value)}>
+            <option value="EUR">EUR</option>
+            <option value="USD">USD</option>
+            <option value="JPY">JPY</option>
+          </select>
+          <p className="price">{loading ? "Laden..." : error ? "-" : price}</p>
+        </div>
+      </form>
+
+      {error && <p className="error">Oops er is iets fout gegaan, probeer het later opnieuw.</p>}
     </div>
   );
 }
